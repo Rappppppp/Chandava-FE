f142439de36d18b0371596d039c68b32dbe9e741
aro <arolatenci@gmail.com>
2025-11-30 13:47:46 +0800

    Revert CSRF Token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 // /src/services/api.ts
+import { getCookie } from "@helpers/cookieHelper";
 import axios, {
   AxiosInstance,
   AxiosRequestConfig,
@@ -6,24 +7,37 @@ import axios, {
   AxiosError,
 } from "axios";
 
+
 const api: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/${import.meta.env.VITE_API_VERSION}`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
-  xsrfCookieName: "XSRF-TOKEN",
-  xsrfHeaderName: "X-XSRF-TOKEN",
+  xsrfCookieName: "XSRF-TOKEN",       // Laravel default
+  xsrfHeaderName: "X-XSRF-TOKEN",     // Laravel expects this header
 });
+
+api.interceptors.request.use((config) => {
+  const token = getCookie("XSRF-TOKEN");
+  if (token && config.headers) {
+    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+  }
+  return config;
+});
+
+
 
 const request = async <T>(config: AxiosRequestConfig): Promise<T> => {
   try {
     const response: AxiosResponse<T> = await api.request<T>(config);
     return response.data;
   } catch (error) {
-    throw error as AxiosError;
+    const axiosError = error as AxiosError;
+    throw axiosError;
   }
 };
+
 
 export { request, AxiosError };
 export default api;
