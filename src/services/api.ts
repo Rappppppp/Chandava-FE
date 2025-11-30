89d118e21ecec0ddd69a5251f3c1add55c19a7c8
aro <arolatenci@gmail.com>
2025-11-30 13:10:51 +0800

    CSRF Token Update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,4 @@
 // /src/services/api.ts
-import { getCookie } from "@helpers/cookieHelper";
 import axios, {
   AxiosInstance,
   AxiosRequestConfig,
@@ -7,37 +6,24 @@ import axios, {
   AxiosError,
 } from "axios";
 
-
 const api: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/${import.meta.env.VITE_API_VERSION}`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
-  xsrfCookieName: "XSRF-TOKEN",       // Laravel default
-  xsrfHeaderName: "X-XSRF-TOKEN",     // Laravel expects this header
+  xsrfCookieName: "XSRF-TOKEN",
+  xsrfHeaderName: "X-XSRF-TOKEN",
 });
-
-api.interceptors.request.use((config) => {
-  const token = getCookie("XSRF-TOKEN");
-  if (token && config.headers) {
-    config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
-  }
-  return config;
-});
-
-
 
 const request = async <T>(config: AxiosRequestConfig): Promise<T> => {
   try {
     const response: AxiosResponse<T> = await api.request<T>(config);
     return response.data;
   } catch (error) {
-    const axiosError = error as AxiosError;
-    throw axiosError;
+    throw error as AxiosError;
   }
 };
-
 
 export { request, AxiosError };
 export default api;
